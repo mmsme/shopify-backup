02c85716e5ba0e3f02cd01b57aa8e69eda3a6ebb
mmsme <mmsmefcis@gmail.com>
2021-05-19 14:11:27 +0200

    add-fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './Navbar/navbarComponent/search/search.component';
+import { SubNavbarComponent } from './Navbar/navbarComponent/sub-navbar/sub-navbar.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SubNavbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
