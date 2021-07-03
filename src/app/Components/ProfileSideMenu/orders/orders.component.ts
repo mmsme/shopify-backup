700d2aee1bc33b714ac4775a67eebd078ab5cd35
Aya-Mo <57008967+Aya-Mo@users.noreply.github.com>
2021-07-04 01:17:04 +0200

    final

--- src/app/Components/ProfileSideMenu/orders/orders.component.ts
+++ src/app/Components/ProfileSideMenu/orders/orders.component.ts
@@ -10,10 +10,10 @@ export class OrdersComponent implements OnInit {
   Element1= document.getElementById("approved");
   Element2 = document.getElementById("shipped");
   Element3= document.getElementById("arrived");
-  flag: any = true;
+  flag: any = false;
   flag2: any = false;
   carts: any = [];
-  
+  closedOrders = [];
   constructor(private http:HttpClient) { }
 httpOptions = {
     headers: new HttpHeaders().set(
@@ -24,7 +24,27 @@ httpOptions = {
   ngOnInit(): void {
     this.http.get<any>("http://localhost:23873/api/cart/customer-payed", this.httpOptions).subscribe(
       a => {
-        this.carts = a;
+        // this.carts = a;
+        if (a.length > 0) {
+          for (let i = 0; i < a.length; i++) { 
+             if (a[i].status.statusName != 'Arrived') {
+              this.flag = true;
+              this.carts.push(a[i])
+              
+            }
+            else if (a[i].status.statusName == 'Arrived') {
+              this.flag2 = true;
+              this.closedOrders.push(a[i])
+              
+            }
+          }
+          console.log("open",this.carts)
+                console.log("close", this.closedOrders)
+        }
+        else {
+          this.flag = false;
+          this.flag2=false
+        }
         console.log(a)
       }
     )
--- src/app/Pages/Register/customer-register/customer-register.component.ts
+++ src/app/Pages/Register/customer-register/customer-register.component.ts
@@ -19,12 +19,12 @@ export class CustomerRegisterComponent implements OnInit {
   Registerform = new FormGroup({
     Fname: new FormControl('',[Validators.required,Validators.minLength(3)]),
     Lname:new FormControl('',[Validators.required,Validators.minLength(3)]),
-    Username:new FormControl('',Validators.required),
     Gender:new FormControl('',Validators.required),
     Address :new FormControl('',Validators.required),
     Email: new FormControl('',[Validators.required,Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
     Password: new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]),
-  });
+  }
+  );
   OnSubmit() {
       this.customerService.postCustomerData(this.Registerform.value).subscribe(
         result => {this.data=result
